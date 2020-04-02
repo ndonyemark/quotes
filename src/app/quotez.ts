9aa0eda198da1dc9be47ed054544fe1b8bec45dd
ndonye mark <ndonyemark@gmail.com>
2020-04-02 23:19:29 +0300

    added a class quote to create a blue print of our app

--- src/app/quotez.ts
+++ src/app/quotez.ts
@@ -1,2 +1,12 @@
 export class Quotez {
+  constructor(public id: number,
+    public quote:string,
+    public author: string,
+    public submitter:string,
+    public datePosted: Date,
+    public likes: number,
+    public dislikes:number
+    ){
+
+  }
 }
